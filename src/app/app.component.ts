29ec44fca0e4f6050230a30aa1784df5fb51ec6f
Ryan Oakley <yelkao85@gmail.com>
2018-11-14 21:34:56 -0400

    does this work now

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,7 +10,7 @@ import { Socket } from "socket.io-client";
 
 export class AppComponent {
 
-  private SOCKET_IO_URL:string = "http://localhost:8080";
+  // private SOCKET_IO_URL:string = "http://localhost:8080";
   // private socket:socketIO.Socket
   // The socket object to rep the socket between client and server
   private socket:Socket;
@@ -22,7 +22,8 @@ export class AppComponent {
 
   ngOnInit():void {
     // Connect to the socket.io server
-    this.socket = socketIO.connect(this.SOCKET_IO_URL);
+    // this.socket = socketIO.connect(this.SOCKET_IO_URL);
+    this.socket = socketIO.connect();
 
     // wire up and event listener to listen for the data received
     this.socket.on("dataReceived", (data) => {
