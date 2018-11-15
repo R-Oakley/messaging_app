34625ee467c39791e121202822dc6fcab5b1254c
Ryan Oakley <yelkao85@gmail.com>
2018-11-14 20:26:52 -0400

    first commit

---
+++ server/src/Server.js
@@ -0,0 +1,32 @@
+// require all the modules
+let express = require("express");
+let cors = require('cors');
+let app = express();
+
+// setup cors module middleware with express
+app.use(cors());
+// setup middleware for static files location
+app.use(express.static('./dist'));
+
+// Socket.io has to work with an HTTP server - so creating HTTP server based on express server
+let server = require("http").Server(app);
+
+// Constructing socket.io object and wire up to our HTTP server
+let socketIOServer = require("socket.io")(server);
+
+socketIOServer.on("connection", (socket) => {
+  console.log("A user connected!");
+
+  // Listening for DataSent data coming through the socket
+  socket.on("dataSent", (data) => {
+    console.log("Received from browser: " + data.message);
+
+    // broadcasting the data to all the other browsers
+    socket.broadcast.emit("dataReceived", data);
+    // Send data back to the browser that sent it
+    socket.emit("dataReceived", data);
+  });
+
+});
+
+server.listen(8080, () => console.log("Listening on port 8080"));
---
+++ src/app/app.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import * as socketIO from "socket.io-client";
+import { Socket } from "socket.io-client";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+
+export class AppComponent {
+
+  private SOCKET_IO_URL:string = "http://localhost:8080";
+  // private socket:socketIO.Socket
+  // The socket object to rep the socket between client and server
+  private socket:Socket;
+
+  // The input message from the user
+  public input:string;
+
+  public messages:string[] = [];
+
+  ngOnInit():void {
+    // Connect to the socket.io server
+    this.socket = socketIO.connect(this.SOCKET_IO_URL);
+
+    // wire up and event listener to listen for the data received
+    this.socket.on("dataReceived", (data) => {
+      console.log("Received from server: " + data.message);
+
+      // save the message in an array
+      this.messages.unshift(data.message);
+    });
+
+  }
+
+  public submit() {
+
+    // User click the OK button
+    this.socket.emit("dataSent",{"message": this.input});
+    this.input = "";
+
+  }
+
+}
